75282da2715c7dbe5a20942bca3fea38e0beee2e
jn-prod <nicolas.jouanno@gmail.com>
2019-09-23 10:07:47 +0200

    init scraper for scrapinghub test

---
+++ vttbzh_scraper/spiders/nafix.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+import os
+import datetime
+
+class NafixSpider(scrapy.Spider):
+    now = datetime.datetime.now()
+    year = int(now.year)
+    name = 'nafix'
+    domain = 'https://www.nafix.fr'
+    url = str(domain) + '/sorties/vtt/' + str(year) + '-avenir-56-29-22-35-44-0-0-0-1.html'
+    start_urls = [url]
+
+    def parse(self, response):
+        for href in response.css('a'):
+            if href.css('a ::text').get() == 'VOIR':
+                response = href.css('a::attr(href)').get()
+                next_page = self.domain + '/' + response.split('/')[2] + '/' + response.split('/')[3]
+                yield scrapy.Request(next_page, callback=self.parse_event, dont_filter=True)
+
+        next_page = str(self.domain) + '/sorties/vtt/' + str(self.year+1) + '-avenir-56-29-22-35-44-0-0-0-1.html'
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_event(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+        yield {
+            'name': extract_with_css('#txt_ref_int_nom_2 ::text'),
+            'city': extract_with_css('#txt_ref_int_lieu_2 ::text'),
+            'departement': extract_with_css('#txt_ref_int_dpt_2 ::text'),
+            'date': extract_with_css('#txt_ref_int_date_2 ::text'),
+            'organisateur': extract_with_css('#txt_ref_int_organisateur_2 ::text'),
+            'hour': extract_with_css('#txt_ref_int_horaires_2 ::text'),
+            'website': extract_with_css('#StyleLien1 ::text'),
+            'price': extract_with_css('#txt_ref_int_prix2 ::text'),
+            'contact': extract_with_css('#txt_ref_int_contacttxt ::text'),
+            'description': extract_with_css('#txt_ref_int_decription ::text')
+        }
+
+# class NafixSpider(scrapy.Spider):
+#     name = 'nafix'
+#     allowed_domains = ['nafix.com']
+#     start_urls = ['http://nafix.com/']
+
+#     def parse(self, response):
+#         pass
